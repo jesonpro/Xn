b854e4e6eac9d5c6cc144f8f6d8a0fa805abde11
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-09 22:22:22 +0530

    Create events.js

--- events.js
+++ events.js
@@ -1,17 +1,10 @@
-/* Copyright (C) 2020 Yusuf Usta.
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
-*/
 
-// Komutları burada tutacağız.
 var config = require('./config');
 var Commands = [];
 
 function addCommand(info, func) {
-    // Basit bir fonksiyon, komut eklemek için.
     var types = ['photo', 'image', 'text', 'message'];
 
     var infos = {
